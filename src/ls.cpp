00683f316ece85722cfcac4c752015ca804f71ff
Samuel Rufer <srufe001@hammer.cs.ucr.edu>
2015-05-01 09:54:17 -0700

    -l works properly, added file coloring

--- src/ls.cpp
+++ src/ls.cpp
@@ -2,8 +2,11 @@
 //Do I have to delete stuff like the stat object
 //TODO IMPORTANT: do I need case-insensitive sorting
 //TODO block size appears to be doubled. idk what that's about
+//what is "do_ypcall" error?
+//Do I need characters like "/" or "*" and coloring for stuff like sockets
 
 #include<iostream>
+#include<iomanip>
 #include<unistd.h>
 #include<stdio.h>
 #include<sys/types.h>
@@ -14,6 +17,9 @@
 #include<string>
 #include<cstring>
 #include<time.h>
+#include<pwd.h>
+#include<grp.h>
+#include<cerrno>
 
 using namespace std;
 
@@ -25,22 +31,6 @@ bool show_hidden = false;
 bool recursive = false;
 bool more_info = false;
 
-/* Potential helper functions */// - remember to check if c++ already has these
-// string withColor(const string& file, struct stat s)
-// padding function
-// columns function!!!
-//    see cout << setw(x) << left/right
-//    You will still need to find the column widths, but this'll be easy for -l
-//    For not -l, how can I tell how many columns to use? Trial and error?
-//    What should it even look like? I need to investigate this further, maybe
-//    read the source code of ls
-// function for outputting a single file in -l
-
-// prints info on each file specified by the strings contained in the vector
-// "files", also prints subdirectories recursively if -R flag is enabled
-// TODO should I clean output of files that don't exist? I will never pass
-// nonexistant files into this in the current form of my program
-
 unsigned numDigits(int n)
 {
    unsigned digits = 0;
@@ -51,9 +41,55 @@ unsigned numDigits(int n)
    return digits;
 }
 
-void print_files(vector<string>& files)
+string fileName(string& file, struct stat& s)
+{
+   string filename;
+
+   if (file.size() > 0 && file.at(0) == '.')
+      filename += "\x1b[47m";
+
+   if (s.st_mode & S_IFREG) {
+      if (s.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH))  // file is executable
+         filename += "\x1b[32m" + file + "\x1b[0m" + '*';
+      else
+         filename += file;
+   } else if (s.st_mode & S_IFDIR) {
+      filename += "\x1b[34m" + file + "\x1b[0m" + '/';
+   } else if (s.st_mode & S_IFCHR) {
+      filename += file;
+   } else if (s.st_mode & S_IFBLK) {
+      filename += file;
+   } else if (s.st_mode & S_IFIFO) {
+      filename += file;
+   } else if (s.st_mode & S_IFLNK) {
+      filename += file + '@';
+   } else if (s.st_mode & S_IFSOCK) {
+      filename += file;
+   }
+   return filename + "\x1b[0m";
+}
+
+void print_files(vector<string>& files, string dirname = ".", bool printBlockSize = true)
 {
    sort(files.begin(), files.end());
+
+   // get stat objects for everything
+   vector<struct stat> stats;
+   vector<bool> stat_exists;
+   for (unsigned i = 0; i < files.size(); ++i)
+   {
+      struct stat temp;
+      string filename = dirname + "/" + files.at(i);
+      if (-1 == stat(filename.c_str(), &temp))
+      {
+         string errorMessage = "unable to open file " + files.at(i);
+         perror(errorMessage.c_str());
+         stat_exists.push_back(false);
+         continue;
+      }
+      stats.push_back(temp);
+      stat_exists.push_back(true);
+   }
 
    unsigned term_width = 80;
    if (!more_info)
@@ -63,38 +99,23 @@ void print_files(vector<string>& files)
       {
          if (output.size() == 0)
          {
-            output += files.at(i);
+            output += fileName(files.at(i), stats.at(i));
          } else if (output.size() + 2 + files.at(i).size() > term_width) {
             cout << output << '\n';
-            output = files.at(i);
+            output = fileName(files.at(i), stats.at(i));
          } else {
-            output += "  " + files.at(i);
+            output += "  " + fileName(files.at(i), stats.at(i));
+
          }
       }
       cout << output << endl;
    } else {
-      // get stat objects for everything
-      vector<struct stat> stats;
-      vector<bool> stat_exists;
-      for (unsigned i = 0; i < files.size(); ++i)
-      {
-         struct stat temp;
-         if (-1 == stat(files.at(i).c_str(), &temp))
-         {
-            perror("unable to open file");   // TODO make more descriptive
-            stat_exists.push_back(false);
-            continue;
-         }
-         stats.push_back(temp);
-         stat_exists.push_back(true);
-      }
-
       // calculate unknown column widths and number of hard links
       unsigned total_blocks = 0;
-      //int max_user_name_length = 0;
-      //int max_group_name_length = 0;
+      unsigned max_user_name_length = 0;
+      unsigned max_group_name_length = 0;
       unsigned max_linked_files_length = 0;
-      unsigned max_bit_size_length = 0;
+      unsigned max_byte_size_length = 0;
       //int max_date_length = 0;
       for (unsigned i = 0; i < files.size(); ++i)
       {
@@ -104,22 +125,41 @@ void print_files(vector<string>& files)
 
             total_blocks += filestat.st_blocks;
 
-            if (stats.at(i).st_size > max_bit_size_length)
-               max_bit_size_length = stats.at(i).st_size;
-
-            if (stats.at(i).st_size > max_bit_size_length)
-               max_bit_size_length = stats.at(i).st_size;
+            if (filestat.st_size > max_byte_size_length)
+               max_byte_size_length = filestat.st_size;
 
             if (filestat.st_nlink > max_linked_files_length)
                max_linked_files_length = filestat.st_nlink;
 
-            if (filestat.st_size > max_bit_size_length)
-               max_bit_size_length = filestat.st_size;
+            struct passwd * username;
+            if (NULL == (username = getpwuid(filestat.st_uid)))
+            {
+               perror("unable to get username");
+            } else {
+               if (strlen(username->pw_name) > max_user_name_length)
+                  max_user_name_length = strlen(username->pw_name);
+            }
+
+            struct group * groupname;
+            if (NULL == (groupname = getgrgid(filestat.st_gid)))
+            {
+               perror("unable to get groupname");
+            } else {
+               if (strlen(groupname->gr_name) > max_group_name_length)
+                  max_group_name_length = strlen(groupname->gr_name);
+            }
          }
       }
       // calculate digits of max_bit_size_length and max_linked_files_length
-      max_bit_size_length = numDigits(max_bit_size_length);
+      max_byte_size_length = numDigits(max_byte_size_length);
       max_linked_files_length = numDigits(max_linked_files_length);
+      // total_blocks is in 512 byte blocks, this shouldbe 1k bype blocks
+      // TODO check if this gives the right output
+      total_blocks /= 2;
+
+      // output block size
+      if (printBlockSize)
+         cout << "total: " << total_blocks << '\n';
       
       // output data
       for (unsigned i = 0; i < files.size(); ++i)
@@ -128,34 +168,22 @@ void print_files(vector<string>& files)
          {
             struct stat & filestat = stats.at(i);
 
-            // determine file type
-            char filetype[2];
-            string color;
+            // print filetype
             if (filestat.st_mode & S_IFREG) {
-               filetype[0] = '-';
-               filetype[1] = '\0';
+               cout << '-';
             } else if (filestat.st_mode & S_IFDIR) {
-               filetype[0] = 'd';
-               filetype[1] = '/';
+               cout << 'd';
             } else if (filestat.st_mode & S_IFCHR) {
-               filetype[0] = 'c';
-               filetype[1] = '\0';
+               cout << 'c';
             } else if (filestat.st_mode & S_IFBLK) {
-               filetype[0] = 'b';
-               filetype[1] = '\0';
+               cout << 'b';
             } else if (filestat.st_mode & S_IFIFO) {
-               filetype[0] = 'p';
-               filetype[1] = '\0';
+               cout << 'p';
             } else if (filestat.st_mode & S_IFLNK) {
-               filetype[0] = 'l';
-               filetype[1] = '\0';
+               cout << 'l';
             } else if (filestat.st_mode & S_IFSOCK) {
-               filetype[0] = 's';
-               filetype[1] = '\0';
+               cout << 's';
             }
-
-            // print file type
-            cout << filetype[0];
 
             // print permissions
             cout << ((filestat.st_mode & S_IRUSR) ? 'r' : '-')
@@ -170,6 +198,41 @@ void print_files(vector<string>& files)
 
             cout << ' ';
 
+            // print number of hard links pointing to this file
+            cout << setw(max_linked_files_length) << right << filestat.st_nlink;
+
+            cout << ' ';
+
+            // print owner's username
+            struct passwd * username;
+            if (NULL == (username = getpwuid(filestat.st_uid)))
+            {
+               string errorMessage = "unable to get username for uid " + filestat.st_uid;
+               perror(errorMessage.c_str());
+            } else {
+               cout << setw(max_user_name_length) << right << username->pw_name;
+            }
+
+            cout << ' ';
+
+            // print owner's group
+            struct group * groupname;
+            if (NULL == (groupname = getgrgid(filestat.st_gid)))
+            {
+               string errorMessage = "unable to get groupname for guid " + filestat.st_gid;
+               perror(errorMessage.c_str());
+            } else {
+               cout << setw(max_group_name_length) << right << groupname->gr_name;
+            }
+
+            cout << ' ';
+
+            // print byte size
+            cout << setw(max_byte_size_length) << right << filestat.st_size;
+            
+            cout << ' ';
+
+            // print the date of last status change
             time_t time = filestat.st_ctime;
             struct tm* timeptr = localtime(&time);
             if (timeptr == NULL)
@@ -180,9 +243,8 @@ void print_files(vector<string>& files)
 
             cout << ' ';
 
-
             // print file name
-            cout << files.at(i);
+            cout << fileName(files.at(i), stats.at(i));
             cout << '\n';
          }
       }
@@ -201,30 +263,37 @@ void print_contents(const string& dir)
    DIR * dirstream; 
    if (NULL == (dirstream = opendir(dir.c_str())))
    {
-      perror("unable to open directory"); // TODO make more descriptive 
+      string errorMessage = "unable to open directory " + dir;
+      perror(errorMessage.c_str());
       return;
    }
 
    // create a list of the files contained in this directory
    vector<string> files;
 
+   int tmp_old_errno = errno;
    struct dirent * dent;
    while (NULL != (dent = readdir(dirstream)))
    {
       if (!show_hidden && strlen(dent->d_name) > 0 && dent->d_name[0] == '.')
          continue;
-      files.push_back(string(dent->d_name));
+      files.push_back(dent->d_name);
    }
-   // TODO you will have to check if errno got set an call perror
+   if (errno != tmp_old_errno)
+   {
+      string errorMessage = "unable to read from file " + dir;
+      perror(errorMessage.c_str());
+   }
    
    if (-1 == closedir(dirstream))
    {
-      perror("failed to close directory"); // TODO make more descriptive
+      string errorMessage = "failed to close directory " + dir;
+      perror(errorMessage.c_str());
       return;
    }
 
    // print files. This also alphabetizes and does recursive printing if -R flag is used
-   print_files(files);
+   print_files(files, dir);
 }
 
 // prints the directory
@@ -237,12 +306,6 @@ void print_directory(const string& dir)
 int main(int argc, char** argv)
 {
    /* Process flags */
-
-   /* TODO REMOVE
-   cout << "arguments before getopt: ";
-   PRINTARR(argv, argc)
-   */
-
    // Set the proper booleans based on the flags, probably use getopts.
    int flag;
    while (-1 != (flag = getopt(argc, argv, "Ral")))
@@ -263,12 +326,6 @@ int main(int argc, char** argv)
    }
 
    /* Remove flag arguments */
-
-   /* TODO REMOVE
-   cout << "arguments after getopts: ";
-   PRINTARR(argv, argc)
-   */
-
    argc -= optind; // set number of arguments to the number of non-flag arguments
    argv += optind; // getopt rearranges all non-flag arguments to the end of argv, so the
                    // following line points argv at the first of these arguments
@@ -304,16 +361,6 @@ int main(int argc, char** argv)
    // sort directories and files alphabetically
    sort(dirs.begin(), dirs.end());
    sort(files.begin(), files.end());
-
-   /* TODO REMOVE
-   cout << "dirs and files after sorting: ";
-   for (unsigned i = 0; i < dirs.size(); ++i)
-      cout << dirs.at(i) << " ";
-   cout << endl;
-   for (unsigned i = 0; i < files.size(); ++i)
-      cout << files.at(i) << " ";
-   cout << endl;
-   */
    
    /* Print output */
 
@@ -325,7 +372,7 @@ int main(int argc, char** argv)
       // files first
       if (files.size() > 0)
       {
-         print_files(files);
+         print_files(files, ".", false);
          if (dirs.size() > 0) cout << "\n";
       }
       // then directories
